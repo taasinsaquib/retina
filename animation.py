01eef71d9f72e4bf84fa753975ba7e46708ac3b5
Taasin <taasin.saquib@gmail.com>
2021-07-19 14:59:10 -0700

    create func to move sphere and collect data

--- animation.py
+++ animation.py
@@ -90,7 +90,7 @@ def rotate_view(vis):
 # eyeVis.destroy_window()
 
 # Two windows at once
-
+"""
 # np.set_printoptions(threshold=sys.maxsize)
 np.set_printoptions(threshold = False)
 
@@ -181,3 +181,129 @@ np.save('data/labels', labels)
 
 scene.destroy_window()
 eyeVis.destroy_window()
+"""
+
+# for horizontal motion
+def generateTrainingDataH(dataName, eyeCenter, delta, w=200, h=200, c=3):
+
+    # lineset to see boundaries of viewing frustum
+    """
+    points = [
+        [1.5, 0, 0],
+        [-1.5, 0, 0],
+        [0, 1.5, 0],
+        [0, -1.5, 0],
+    ]
+
+    lines = [
+        [0, 1],
+        [2, 3]
+    ]
+
+    line_set = o3d.geometry.LineSet(
+        points=o3d.utility.Vector3dVector(points),
+        lines=o3d.utility.Vector2iVector(lines),
+    )
+    """
+
+    # create sphere
+    mesh_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.5)
+    mesh_sphere.compute_vertex_normals()
+    mesh_sphere.paint_uniform_color([0.1, 0.1, 0.7])
+
+    # create scene, add sphere to it
+    scene = o3d.visualization.Visualizer()
+    scene.create_window(window_name='Main Scene', width=w, height=h, left=200, top=500, visible=True)
+    scene.add_geometry(mesh_sphere)
+    # scene.add_geometry(line_set)
+
+    sceneControl = scene.get_view_control()
+    # print(sceneControl.get_field_of_view())
+    # sceneControl.set_constant_z_far(-2)
+    # sceneControl.camera_local_translate(forward=0., right=0., up=0.)
+    sceneControl.set_zoom(1.5)
+
+    lastImage = None
+    curDisplay = o3d.geometry.Image()
+
+    dataSet = []
+    labels = []
+
+    # figure out how many steps to take based on dx
+    distX = 1.5 - (-1.5) + 2 * 0.25      # maxX - minX + 2 * radius of sphere + little extra
+    nStepsX = distX / delta + 2 * 0.5
+
+    distY = 1.5 - (-1.5) + 2 * 0.25 + 0.2     # maxX - minX + 2 * radius of sphere + little extra
+    nStepsY = distY / delta
+
+    mesh_sphere.translate((-delta*nStepsX/2, -delta*nStepsY/2, 0))
+
+    print(nStepsY, nStepsX)
+
+    for i in range(0, int(nStepsY)):
+        mesh_sphere.translate((0, delta, 0))
+
+        for j in range(0, int(nStepsX)):
+            mesh_sphere.translate((delta, 0, 0))
+            # print(mesh_sphere.get_center())
+
+            scene.update_geometry(mesh_sphere)
+            scene.poll_events()
+            scene.update_renderer()
+            
+            curImage = scene.capture_screen_float_buffer(do_render=True)
+            
+            # scene.capture_screen_image(f'/Users/Saquib/Desktop/{i}.png', do_render=True)
+
+            # display the difference between the frames
+            if lastImage != None:
+
+                events = np.asarray(curImage, dtype=np.int8) - np.asarray(lastImage, dtype=np.int8)
+
+                # make binary events
+                binEvents = np.zeros((w, h))
+
+                bright = np.argwhere(events > 0)
+                dim    = np.argwhere(events < 0)
+
+                binEvents[bright[:, :2]] = 1
+                binEvents[dim[:, :2]]    = -1
+
+                # label
+                [sX, sY, _] = mesh_sphere.get_center()
+                if (abs(sX) > 1.5 + 0.25) or (abs(sY) > 1.5 + 0.25):    # deltaGaze zero'd out if sphere not in view
+                    deltaGaze = [0, 0, 0]
+                else:
+                    deltaGaze = mesh_sphere.get_center() - eyeCenter
+
+                dataSet.append(binEvents)
+                labels.append(deltaGaze)
+
+                # print(np.count_nonzero(events < 0), np.count_nonzero(events > 0), np.count_nonzero(events))
+                # print(len(dim), len(bright))
+
+                # for visualization purposes
+                events[events>0] = 150
+
+                # convert to b/w for binary events
+                # bw = np.dot(events[...,:3], [0.299, 0.587, 0.114])
+
+                curDisplay = o3d.geometry.Image(events)
+                # o3d.io.write_image(f'/Users/Saquib/Desktop/{j}.png', curDisplay)
+
+                # eyeVis.update_geometry(curDisplay)
+                # eyeVis.poll_events()
+                # eyeVis.update_renderer()
+
+            lastImage = curImage
+        
+        mesh_sphere.translate((-nStepsX * delta, 0, 0))
+
+    # save data
+    # np.save('data/data', dataSet)
+    # np.save('data/labels', labels)
+
+    scene.destroy_window()
+
+eyeCenter = [0, 0, 3]
+generateTrainingDataH("hi", eyeCenter, 0.1)
