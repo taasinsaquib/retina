371cacea5a2f505f2d1cf3b68d4ebdcd73daca7a
Taasin <taasin.saquib@gmail.com>
2021-08-09 11:41:58 -0700

    workflow for moving the sphere around a circle

--- axisRotation.py
+++ axisRotation.py
@@ -5,6 +5,8 @@ from scipy.spatial.transform import Rotation as R
 
 from tryCasting import vecAngle
 
+np.set_printoptions(suppress=True)
+
 w = 600
 h = 600
 
@@ -52,7 +54,6 @@ line_set.colors = o3d.utility.Vector3dVector(colors)
 
 scene.add_geometry(line_set)
 
-
 dx = 0.01
 dy = 0.00
 
@@ -71,44 +72,82 @@ curCenter  = None
 
 pupil = [0, 0, 0]
 
-while True:
-    mesh_sphere.translate(delta)
-    curCenter = mesh_sphere.get_center()
+def followSphere():
+    while True:
+        mesh_sphere.translate(delta)
+        curCenter = mesh_sphere.get_center()
 
-    if prevCenter is not None:
-        x, y, z = vecAngle(pupil, axes, prevCenter, curCenter)
-        
-        print(x * 180/np.pi, y * 180/np.pi, z * 180/np.pi)
+        if prevCenter is not None:
+            x, y, z = vecAngle(pupil, axes, prevCenter, curCenter)
+            
+            print(x * 180/np.pi, y * 180/np.pi, z * 180/np.pi)
 
-        print(poly*z, polx*y, x)
-        r = R.from_euler('xyz', [poly*z, polx*-y, x])
-        
-        ax = np.array(axes) @ np.array(r.as_matrix()).T
+            print(poly*z, polx*y, x)
+            r = R.from_euler('xyz', [poly*z, polx*-y, x])
+            
+            ax = np.array(axes) @ np.array(r.as_matrix()).T
 
-        points[1] = ax[0]
-        points[2] = ax[1]
-        # points[3] = ax[2]
+            points[1] = ax[0]
+            points[2] = ax[1]
+            # points[3] = ax[2]
 
-        v1 = curCenter - pupil
-        v1 = v1 / np.linalg.norm(v1)
-        points[3] = v1
+            v1 = curCenter - pupil
+            v1 = v1 / np.linalg.norm(v1)
+            points[3] = v1
 
-        points[4] = curCenter
+            points[4] = curCenter
 
-        # rotate around cur Axes
-        line_set.points = o3d.utility.Vector3dVector(points)
+            # rotate around cur Axes
+            line_set.points = o3d.utility.Vector3dVector(points)
 
-        colors = [[0, 1, 0] for i in range(len(lines))]
-        line_set.colors = o3d.utility.Vector3dVector(colors)
+            colors = [[0, 1, 0] for i in range(len(lines))]
+            line_set.colors = o3d.utility.Vector3dVector(colors)
 
-        # rotate axes
+            # rotate axes
 
-    prevCenter = curCenter
+        prevCenter = curCenter
+
+        scene.update_geometry(mesh_sphere)
+        scene.update_geometry(line_set)
+
+        scene.poll_events()
+        scene.update_renderer()
+
+        # input("hi")
+
+r = 1
+
+# x^2 + y^2 = r^2
+
+x = np.arange(-r, r+0.1, 0.1)
+y = np.sqrt(r**2 - np.square(x))    # plus/minus
+
+x = np.hstack((x, x))
+y = np.hstack((y, -1*y))
+
+points = zip(x, y)
+
+for px, py in points:
+    mesh_sphere.translate([px, py, 0])
 
     scene.update_geometry(mesh_sphere)
-    scene.update_geometry(line_set)
-
     scene.poll_events()
     scene.update_renderer()
 
-    # input("hi")
+    """
+        raycast, collect data and label
+    """
+
+    input("hi")
+    mesh_sphere.translate([-px, -py, 0])
+
+    scene.update_geometry(mesh_sphere)
+    scene.poll_events()
+    scene.update_renderer()
+
+    """
+        reset prev stuff
+        raycast
+    """
+
+    input("hi2")
