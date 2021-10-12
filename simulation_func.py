9b10aba9738997ba6783158a6dd8f8d110c198ff
Taasin <taasin.saquib@gmail.com>
2021-10-12 14:06:42 -0700

    run snn from python file

--- simulation_func.py
+++ simulation_func.py
@@ -24,7 +24,7 @@ class FC(nn.Module):
         x = self.fc2(x)
         return x
 
-"""
+
 class FCSpiking(nn.Module):
   def __init__(self, alpha, beta):
     super(FCSpiking, self).__init__()
@@ -52,43 +52,50 @@ class FCSpiking(nn.Module):
             cur1 = self.fc1(x[step])
             spk1, syn1, mem1 = self.lif1(cur1, syn1, mem1)
 
-            # print(mem4.size())
-            out = self.fc5(mem1)
+            out = self.fc2(mem1)
 
-            # print(self.out_rec.size(), out.size())
             spk1_rec.append(spk1)
             mem1_rec.append(mem1)
             self.out_rec[step] = out
 
         return mem1_rec, spk1_rec, self.out_rec
-"""
+
 
 def func(x):
     """
         onv [arrray] 
     """
 
+    spiking = True
 
     onv = np.array(x, dtype=float)
-    # print(x)
-    # print(type(onv), onv.shape)
 
-    alpha = 0.9
-    beta  = 0.8
+    if spiking == True:
+        onv = np.expand_dims(onv, axis=0)   # (batch, data) (1, 14400)
+    
+        # TODO: turn into spikes, just adding a dimension for now (nSteps is 1 rn)
+        onv = np.expand_dims(onv, axis=1)   # (batch, data) (1, 14400)
 
-    m = FC()
-    m.to(torch.float)
-    m.to(device)
+        alpha = 0.9
+        beta  = 0.8
+        m = FCSpiking(alpha, beta)
+
+    else:
+        m = FC()
+        m.to(torch.float)
+        m.to(device)
+
+    print("ONV Shape", onv.shape)
 
     inputs = torch.from_numpy(onv)
     inputs = inputs.float()
 
-    output = m(inputs)
-    output = output.cpu().detach().numpy()
+    if spiking == True:
+        _, _, output = m(inputs)
+        output = output.cpu().detach().numpy()
+        output = np.squeeze(output)
+    else:
+        output = m(inputs)
+        output = output.cpu().detach().numpy()
 
     return output
-
-"""
-TODO
-    run SNN on onv input
-"""
